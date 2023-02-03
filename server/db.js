a5409043ca5a10c349e438c2760e64f4f48f90c6
Dominik Jones <dominikjo22@gmail.com>
2023-02-03 14:35:09 -0500

    finished project

--- server/db.js
+++ server/db.js
@@ -4,9 +4,10 @@ require('dotenv').config({ path: __dirname + '.env'})
 const pool = new Pool({
   user: process.env.USERNAME,
   password: process.env.PASSWORD,
-  host: process.env.HOST,
-  port: process.env.DBPORT,
+  host: process.env.HOSTNAME,
+  port: process.env.PORT,
   database: process.env.DATABASE,
+  url: process.env.URL
 })
 
 module.exports = pool
